20b3a89a26aaffb167ff1c97e57ef91bd42acd30
StahnHeng <stahncad@gmail.com>
2017-09-30 05:56:50 +0800

    part5-2

--- app/src/main/java/com/huihui/androidorderfood/database/Database.java
+++ app/src/main/java/com/huihui/androidorderfood/database/Database.java
@@ -1,12 +1,14 @@
 package com.huihui.androidorderfood.database;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 
 import com.huihui.androidorderfood.Model.Order;
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,5 +29,42 @@ public class Database extends SQLiteAssetHelper {
 
         String[] sqlSelect = {"ProductName", "ProductId","Quantity","Price","Discount"};
         String sqlTable="OrderDetail";
+
+        qb.setTables(sqlTable);
+        Cursor c = qb.query(db,sqlSelect,null,null,null,null,null);
+
+        final List<Order> result = new ArrayList<>();
+        if(c.moveToFirst())
+        {
+            do{
+                result.add(new Order(c.getString(c.getColumnIndex("ProductId")),
+                c.getString(c.getColumnIndex("ProductName")),
+                        c.getString(c.getColumnIndex("Quantity")),
+                        c.getString(c.getColumnIndex("Price")),
+                        c.getString(c.getColumnIndex("Discount"))
+                ));
+            }while (c.moveToNext());
+        }
+        return result;
+    }
+
+    public void addToCart(Order order)
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        String query = String.format("INSERT INTO OrderDetail(ProductId,ProductName,Quantity,Price,Discount) VALUES('%s','%s','%s','%s','%s');")
+                order.getProductId();
+                order.getProductName();
+                order.getQuantity();
+                order.getPrice();
+                order.getDiscount();
+        db.execSQL(query);
+    }
+
+    public void cleanCart()
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        String query = String.format("DELETE FROM OrderDetail");
+        db.execSQL(query);
+
     }
 }
